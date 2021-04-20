269dd32d46f2300f6890fc1e73a9fd5187d2a2e7
Pranali Mohite <77424192+PranaliMohite0408@users.noreply.github.com>
2021-04-20 21:01:07 +0530

    Add files via upload

---
+++ Positive or Negative/02_Positive or Negative Number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int Num=0;
+
+    printf("\n Enter an Integer number=");
+    scanf("%d",&Num);
+
+    (Num==0)? printf("\n Given number %d is Neutral",Num):(Num>0)? printf("\n Given number is %d Positive",Num):printf("\n Given number is %d Negative",Num);
+
+    printf("\n\n Thanks");
+    getch();
+    return 0;
+}
