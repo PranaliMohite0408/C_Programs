81a02e7d41c7a14fed8d91413cea91f9c79e93e0
Pranali Mohite <77424192+PranaliMohite0408@users.noreply.github.com>
2021-07-29 19:39:34 +0530

    Add files via upload

---
+++ Bitwise Programs/03_Bitwise XOR.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int No1=0, No2=0, Res =0;
+
+    printf("\nEnter a Two Numbers =>");
+    scanf("%d%d",&No1,&No2);
+
+    Res = No1 ^ No2;
+
+    printf("\n Output of Bitwise XOR between => %d ^ %d = %d",No1,No2,Res);
+
+    printf("\n\nThanks!!!");
+
+    getch();
+    return 0;
+}
